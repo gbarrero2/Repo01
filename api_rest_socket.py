f46a6f4ec6682307b928a8a3f97195a66c05673d
gbarrero <giovanni.barrero@usil.pe>
2024-04-14 21:59:05 -0500

    version inicial v1.0

---
+++ api_rest_socket.py
@@ -0,0 +1,26 @@
+# server.py
+from flask import Flask, request, jsonify
+
+app = Flask("prueba")
+
+@app.route('/rpc', methods=['POST'])
+def rpc_server():
+    data = request.get_json()
+    if data is not None and 'method' in data and 'params' in data:
+        method = data['method']
+        params = data['params']
+
+        # Implementa la lógica del servidor RPC aquí
+        if method == 'sumar':
+            result = sum(params)
+        elif method == 'restar':
+            result = params[0] - params[1]
+        else:
+            return jsonify({"error": "Método no encontrado"}), 404
+
+        return jsonify({"result": result})
+    else:
+        return jsonify({"error": "Solicitud incorrecta"}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ cliente_api_rest.py
@@ -0,0 +1,19 @@
+# client.py
+import requests
+
+def rpc_client(method, params):
+    data = {"method": method, "params": params}
+    response = requests.post('http://localhost:5000/rpc', json=data)
+    if response.status_code == 200:
+        result = response.json()
+        return result['result']
+    else:
+        print(f"Error: {response.status_code}")
+        return None
+
+# Ejemplos de llamadas al servidor RPC
+resultado_suma = rpc_client("sumar", [5, 3])
+resultado_resta = rpc_client("restar", [8, 2])
+
+print(f"Resultado de la suma: {resultado_suma}")
+print(f"Resultado de la resta: {resultado_resta}")
